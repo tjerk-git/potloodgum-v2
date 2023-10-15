4e6418b56ef0e41ceaa9c4e5b32dfd7ad6bc5c04
Tjerk Dijkstra <71773101+tjerk-git@users.noreply.github.com>
2023-10-15 20:51:44 +0200

    removed experiments

--- src/app/submitButton.tsx
+++ src/app/submitButton.tsx
@@ -1,12 +1,9 @@
 'use client'
 
-import { experimental_useFormStatus as useFormStatus } from 'react-dom'
-
 export function SubmitButton() {
-  const { pending } = useFormStatus()
 
   return (
-    <button role="button" aria-label="Verstuur contactformulier" type="submit" className="flex cursor-pointer items-center rounded-md border-2 border-black bg-[#bc95d4] px-10 py-3 font-bold shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] transition-all hover:translate-x-[3px] hover:translate-y-[3px] hover:shadow-none" aria-disabled={pending}>
+    <button role="button" aria-label="Verstuur contactformulier" type="submit" className="flex cursor-pointer items-center rounded-md border-2 border-black bg-[#bc95d4] px-10 py-3 font-bold shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] transition-all hover:translate-x-[3px] hover:translate-y-[3px] hover:shadow-none">
       Versturen!
     </button>
   )
